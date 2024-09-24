b55809367d28d0d91f5c673909cf4afe8f7fd845
Saeeda <saeedabatool39@gmail.com>
2024-09-24 14:56:38 -0400

    added in a function that checks to make sure the input value is not empty and also checks to see if entered value is a number or not

--- assets/index.js
+++ assets/index.js
@@ -1,3 +1,22 @@
 const input = document.getElementById("num-input")
 const convertBtn = document.getElementById("convert-btn")
+const errorMsg = document.getElementById("error-message")
 
+convertBtn.addEventListener("click", function () {
+  let inputEl = input.value
+  if (!inputEl) { 
+    console.log("no input");
+    return;
+  }
+  if (isNaN(inputEl)) {
+    errorMsg.textContent = "Please enter a valid number!"
+    errorMsg.style.display = "block"
+    return;
+  } else { 
+    errorMsg.style.display = "none"
+  }
+}); 
+
+function convertUnits () { 
+  
+}
